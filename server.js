14d1ec922a8514947dddcd9339e80c7eebbf3914
Chance Nguyen <chanceknguyen@gmail.com>
2020-10-05 22:03:26 -0500

    Render components with hardcoded data

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.port || 3000;
+
+const server = app.listen(PORT, () => {
+  console.log(' Tenthop server is listening on port 3000')
+})
+
+app.use(express.static('./client'))
