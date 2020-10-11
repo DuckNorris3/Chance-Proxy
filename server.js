2cf82262b945b4d7386761ae980bbbae7b384b15
chanceknguyen <chanceknguyen@gmail.com>
2020-10-11 14:55:40 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.port || 8080;
+const PORT = process.env.port || 3000;
 
 const server = app.listen(PORT, () => {
   console.log(` Tenthop server is listening on port ${PORT}`)
